0dac4d2708e14b43e090e8c31657fdafe0aa5f7b
danielsouzame <danielsouzame@gmail.com>
2025-09-04 21:49:52 -0300

    Quase nenhuma mudança

--- html-js-rafa/script.js
+++ html-js-rafa/script.js
@@ -1,1 +1,20 @@
-alert("Hello world!")
+// alert("Hello world!")
+// alert("oi")
+// alert("tudo bem?")
+// alert("voce vem sempre aqui?")
+
+// let idade
+// idade=18
+// alert(idade)
+
+// idade = "dezoito"
+// alert(idade)
+
+// idade=18.9
+// alert(idade)
+
+const amor = "meu primeiro amor"
+alert(amor)
+
+amor="my second love"
+alert(amor)
