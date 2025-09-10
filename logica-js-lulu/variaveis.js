8cdbdbfee61d389aa24a6d5257790b88a851767f
danielsouzame <danielsouzame@gmail.com>
2025-09-09 22:12:03 -0300

    Aula de variaveis em js

---
+++ logica-js-lulu/variaveis.js
@@ -0,0 +1,25 @@
+//variavel com var, menos usada hoje em dia pois ele permite cria uma variavel com o mesmo nome
+//var nome = "Luciano" //string
+// var nome = "Daniel"
+//nome = "Joao"
+
+//variavel com let, uma das mais usada hoje em dia pois não deixa criar uma variavel de mesmo nome e é possivel alterar o valor da variavel se necessario
+// let nome = "Daniel"
+// nome = "João"
+
+// variavel com const, variavel constante, não altera o valor em nenhum momento do codigo
+const nome = "Daniel" //string
+const sobrenome = "Medeiros"
+const idade = 104/4 //number
+const masculino = true //boolean
+
+//array
+const frutas = ["laranja","maçã","banana"]
+
+console.log(frutas[0])
+
+// console.log("meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos")
+// console.log(`Meu nome é ${nome} ${sobrenome}, minha idade é ${idade} anos`)
+
+// console.log(idade)
+// console.log(masculino)
