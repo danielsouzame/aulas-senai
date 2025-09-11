2dfe10d32d546691c1ba9465810e08a731118e16
danielsouzame <danielsouzame@gmail.com>
2025-09-11 20:57:03 -0300

    +1 exercicio feito.

--- exercicios-de-js-aula-rafa/mensagem-secreta.js
+++ exercicios-de-js-aula-rafa/mensagem-secreta.js
@@ -1,1 +1,8 @@
 // Mensagem Secreta: Peça ao usuário para digitar uma senha e, se a senha for "abracadabra", exiba uma mensagem secreta.
+let senha = prompt("Digite a palavra secreta para a mensagem secreta: ")
+
+if (senha == "abracadabra"){
+    alert("A mensagem secreta é: Parabens feio!")
+}else{
+    alert("Palavra incorreta! Tente novamente")
+}
--- exercicios-de-js-aula-rafa/verificador-de-ano-bissexto.js
+++ exercicios-de-js-aula-rafa/verificador-de-ano-bissexto.js
@@ -1,1 +1,3 @@
 // Verificando um Ano Bissexto: Peça ao usuário para inserir um ano e determine se ele é bissexto ou não.
+let ano = Number(prompt("Vamos verificar se o ano é bissexto, digite um ano: "))
+
