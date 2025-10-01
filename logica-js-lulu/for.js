089c6ac7951d7bc272a7bbf3cb6ed0bfb9fd8ad4
danielsouzame <danielsouzame@gmail.com>
2025-09-30 22:03:17 -0300

    Aula de funçoes e laço de repetição while e for

---
+++ logica-js-lulu/for.js
@@ -0,0 +1,42 @@
+//contando de 0 a 10 a cada 1
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+// console.log("Fim")
+
+//contando de 10 a 0 a cada 1
+// for(let i = 10; i >= 0; i--){
+//     console.log(i)
+// }
+// console.log("Fim")
+
+//Contando de 0 a 20 a cada 2
+// for(let i = 0; i <= 20; i+= 2){
+//     console.log(i)
+// }
+// console.log("Fim")
+
+//somando todos os resultados PA(prograssao aritimetica)
+// let soma = 0
+
+// for(let i = 1; i < 100; i++){
+//     soma += i
+// }
+// console.log("O resultado da soma ", soma)
+
+
+
+// let alunos = ['João', 'Maria', 'Joana']
+// alunos.push("Pedro")
+
+// for(let i = 0; i < alunos.length; i++){
+//     console.log('Alunos: ',alunos[i])
+// }
+
+// console.log(alunos.length)
+
+let frutas = ['Banana', 'Pera', 'Maça', 'Goiaba', 'Uva']
+
+for(let i = 0; i < frutas.length; i++){
+    console.log('Fruta: ',frutas[i])
+}
---
+++ logica-js-lulu/funcoes.js
@@ -0,0 +1,20 @@
+// function saudacao(nome){
+//     console.log(`Bom dia,${nome}`)
+// }
+
+// const saudacao = (nome) =>{
+//     console.log(`Bom dia,${nome}`)
+// }
+
+// saudacao("João")
+// saudacao("Maria")
+
+// //=> arrow function
+//const soma = (x,y) => x+y 
+
+//Forma mais Verbosa e necessaria quando precisa testar algo
+const soma = (x,y) =>{   
+    return x+y
+}
+console.log(soma (2,2))
+console.log(soma (2,4))
