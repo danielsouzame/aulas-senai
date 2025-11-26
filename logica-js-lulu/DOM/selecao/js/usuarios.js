bd8d2293dbcf622a9767adfbd879672e51a601e2
DANIEL DE SOUZA MEDEIROS <daniel_de-medeiros@estudante.sesisenai.org.br>
2025-11-25 22:12:40 -0300

    continuação aula DOM

---
+++ logica-js-lulu/DOM/selecao/js/usuarios.js
@@ -0,0 +1,12 @@
+fetch("https://jsonplaceholder.typicode.com/users").then(respose => respose.json()).then(usuarios => {
+    const container = document.getElementById("container")
+    const cards = usuarios.map(usuario => {
+        return `
+        <div class="card">
+            <h2>${usuario.name}</h2>
+        </div>
+        `
+    })
+    container.innerHTML = cards.join('')
+})
+
