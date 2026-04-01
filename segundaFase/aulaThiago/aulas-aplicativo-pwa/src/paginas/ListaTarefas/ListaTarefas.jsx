c0fb1ebac3163b1e85ba87c587d4ce8301eb8140
DANIEL DE SOUZA MEDEIROS <daniel_de-medeiros@estudante.sesisenai.org.br>
2026-03-31 21:04:14 -0300

    continuação ListaTarefas

--- segundaFase/aulaThiago/aulas-aplicativo-pwa/src/paginas/ListaTarefas/ListaTarefas.jsx
+++ segundaFase/aulaThiago/aulas-aplicativo-pwa/src/paginas/ListaTarefas/ListaTarefas.jsx
@@ -2,9 +2,22 @@
 import { useState } from "react";
 import Principal from "../../componentes/Principal/Principal"
 import CampoCustomizado from "../../componentes/CampoCustomizado/CampoCustomizado";
+import BotaoCustomizado from "../../componentes/BotaoCustomizado/BotaoCustomizado"
 
 function ListaTarefas(){
     const [novaTarefa, setNovaTarefa] = useState("");
+
+    const [tarefas, setTarefas] = useState([]);
+
+    const adicionarTarefa = () => {
+        if(novaTarefa.trim() === ""){
+            alert("digite uma nova tarefa")
+            return;
+        }
+        console.log(novaTarefa);
+
+        setTarefas([novaTarefa, ...tarefas]);
+    };
 
     return (
         <Principal>
@@ -12,12 +25,18 @@ function ListaTarefas(){
 
             <CampoCustomizado
                 label="Nova Tarefa"
+                //propriedades não mapeadas
+                id="nova-tarefa"
                 type="text"
                 placeholder="Precione Enter para adicionar"
                 value={novaTarefa}
                 onChange={(event) => {setNovaTarefa(event.target.value)}}
             />
 
+            <BotaoCustomizado tipo="secundario" aoClicar={adicionarTarefa}>+</BotaoCustomizado>
+            
+            
+            {JSON.stringify(tarefas, null, 2)}
         </Principal>
     );
 }
