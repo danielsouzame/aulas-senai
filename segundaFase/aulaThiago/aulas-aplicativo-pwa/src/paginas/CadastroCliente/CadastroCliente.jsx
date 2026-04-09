97481c04a5973f309a11927b44a698bf011bb7a5
DANIEL DE SOUZA MEDEIROS <daniel_de-medeiros@estudante.sesisenai.org.br>
2026-04-08 22:02:33 -0300

    Cadastro de imagem

--- segundaFase/aulaThiago/aulas-aplicativo-pwa/src/paginas/CadastroCliente/CadastroCliente.jsx
+++ segundaFase/aulaThiago/aulas-aplicativo-pwa/src/paginas/CadastroCliente/CadastroCliente.jsx
@@ -3,6 +3,9 @@ import Principal from "../../componentes/Principal/Principal";
 import CampoCustomizado from "../../componentes/CampoCustomizado/CampoCustomizado"
 import BotaoCustomizado from "../../componentes/BotaoCustomizado/BotaoCustomizado";
 import { useState } from "react";
+import { formatarComMascara, MASCARA_CPF, NUMERO_TELEFONE } from "../../utils/formatarComMascar";
+import { validarCPF } from "../../utils/validarCPF";
+import validarEmail from "../../utils/validarEmail";
 
 function CadastroCliente() {
     const [cliente, setCliente] = useState({
@@ -19,16 +22,76 @@ function CadastroCliente() {
 
     return (
         <Principal voltarPara="/lista-clientes" titulo="Cadastro de Cliente">
-            <CampoCustomizado label="nome" value={cliente.nome} onChange={(e) => setCliente({...cliente, nome: e.target.value})}/>
-            <CampoCustomizado label="CPF" />
-            <CampoCustomizado type="date" label="Data Nascimento" />
-            <CampoCustomizado type="tel" label="Celular" />
-            <CampoCustomizado type="email" label="E-mail" />
+            <CampoCustomizado
+                label="nome"
+                value={cliente.nome}
+                onChange={(e) => setCliente({ ...cliente, nome: e.target.value })} />
 
-            <BotaoCustomizado onClick={salvar()} tipo="primario">Salvar</BotaoCustomizado>
+            <CampoCustomizado
+                label="CPF"
+                value={cliente.cpf}
+                onChange={(e) => setCliente({ ...cliente, cpf: formatarComMascara(e.target.value, MASCARA_CPF) })}
+                onBlur={(e) => {
+                    if (e.target.value.trim() && !validarCPF(e.target.value)) {
+                        alert('CPF inválido!')
+                    }
+                }}
+            />
+
+            <CampoCustomizado
+                type="date"
+                label="Data Nascimento"
+                value={cliente.dataNascimento}
+                onChange={(e) => setCliente({ ...cliente, dataNascimento: e.target.value })} />
+
+            <CampoCustomizado
+                type="tel"
+                label="Celular"
+                value={cliente.celular}
+                onChange={(e) => setCliente({ ...cliente, celular: formatarComMascara(e.target.value, NUMERO_TELEFONE) })} />
+
+            <CampoCustomizado
+                type="email"
+                label="E-mail"
+                value={cliente.email}
+                onChange={(e) => setCliente({ ...cliente, email: e.target.value })}
+                onBlur={(e) => {
+                    if (e.target.value.trim() && !validarEmail(e.target.value)) {
+                        alert('E-mail inválido!')
+                    }
+                }}
+            />
+
+            <CampoCustomizado
+                type="file"
+                accept="image/*"
+                label="Foto"
+                onChange={(e) => {
+                    const imagem = e.target.files[0];
+                    if (imagem) {
+                        const reader = new FileReader();
+                        reader.onload = (event) => {
+                            setCliente({ ...cliente, foto: event.target.result });
+                        };
+                        reader.readAsDataURL(imagem);
+                    }
+                }}
+            />
+
+            {cliente.foto && (
+                <div style={{ marginTop: "10px", display: "flex", justifyContent: "center" }}>
+                    <img
+                        src={cliente.foto}
+                        alt="Foto do Cliente"
+                        style={{ width: "150px", height: "150px", objectFit: "cover", borderRadius: "8px" }}
+                    />
+                </div>
+            )}
+
+            <BotaoCustomizado aoClicar={salvar} tipo="primario">Salvar</BotaoCustomizado>
         </Principal>
     );
-    
+
 }
 
 export default CadastroCliente;
